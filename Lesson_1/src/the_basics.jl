6b835c90574ade6f9efefe88cfd7f011ca114bf3
Andrew Henning <Andrew.S.Henning@gmail.com>
2021-04-21 07:24:59 +0100

    Added notes on strings

--- Lesson_1/src/the_basics.jl
+++ Lesson_1/src/the_basics.jl
@@ -4,12 +4,62 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ dafabea2-a263-11eb-3978-e7ac5cc9c872
-println("Hello, world!")
-
 # ╔═╡ 0ae8c0f7-cd49-48f2-9cc6-fe7c2798f643
+begin
+	using PlutoUI
+	PlutoUI.TableOfContents()
+end
 
+# ╔═╡ dafabea2-a263-11eb-3978-e7ac5cc9c872
+md"# Julia Basics"
+
+# ╔═╡ 150aca1f-4240-4b87-9829-4183485c1ad2
+md"## Strings"
+
+# ╔═╡ f66631fc-37ed-430b-9a7f-9c5925a56146
+md"Julia supports two methods for making __strings__. Strings are a basic __data-type__, indicating blocks of text such as characters, words, sentences, paragraphs, and documents. To create strings in Julia, start and end a block of text with double-quotes or triple-double-quotes."
+
+# ╔═╡ 076362fd-65cb-4f06-9669-57c77f09c25f
+md"Example of double-quotes:
+
+```
+    \"Hello, world!\"
+```
+"
+
+# ╔═╡ df4c7835-ee19-45f5-8f7f-3e671790c7b3
+md"Example of triple-double-quotes:
+
+```
+
+    \"\"\"Hello, world!\"\"\"
+```
+"
+
+# ╔═╡ d242cd35-2223-49f1-ab85-58ed24f3a874
+md"## Printing"
+
+# ╔═╡ 457892f9-2a7d-43b2-80f9-e481c5b6183f
+println("Hello, world")
+
+# ╔═╡ bbefecc5-6d99-45e9-935c-f6f557a8cb5e
+md"## Commenting"
+
+# ╔═╡ 1400ff4b-34dd-419a-9b97-95d9a090556e
+md"## Variable Assignment"
+
+# ╔═╡ 7b1ed8dd-4d99-4d04-af36-951f18d03123
+md"## Basic Math Operations"
 
 # ╔═╡ Cell order:
-# ╠═dafabea2-a263-11eb-3978-e7ac5cc9c872
-# ╠═0ae8c0f7-cd49-48f2-9cc6-fe7c2798f643
+# ╟─dafabea2-a263-11eb-3978-e7ac5cc9c872
+# ╟─150aca1f-4240-4b87-9829-4183485c1ad2
+# ╟─f66631fc-37ed-430b-9a7f-9c5925a56146
+# ╟─076362fd-65cb-4f06-9669-57c77f09c25f
+# ╟─df4c7835-ee19-45f5-8f7f-3e671790c7b3
+# ╟─d242cd35-2223-49f1-ab85-58ed24f3a874
+# ╠═457892f9-2a7d-43b2-80f9-e481c5b6183f
+# ╟─bbefecc5-6d99-45e9-935c-f6f557a8cb5e
+# ╟─1400ff4b-34dd-419a-9b97-95d9a090556e
+# ╟─7b1ed8dd-4d99-4d04-af36-951f18d03123
+# ╟─0ae8c0f7-cd49-48f2-9cc6-fe7c2798f643
